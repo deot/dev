3621a7af1312b6ac59d0d63ad94bd74053fa77ff
Mr.Z <zrd0921@qq.com>
2023-03-21 11:06:45 +0800

    chore: zero-config eslint & api-extractor

--- packages/cli/src/shared.ts
+++ packages/cli/src/shared.ts
@@ -1,8 +1,14 @@
+import { createRequire } from "node:module";
+import path from 'node:path';
+
+const require$ = createRequire(import.meta.url);
+
+const cwd = process.cwd();
 export class Shared {
 	static impl() {
 		return {
-			packageName: '@deot/dev',
-			directory: '/Users/deot/Desktop/wya/github/dev/packages'
+			packageName: require$(`${cwd}/packages/index/package.json`).name,
+			directory: path.resolve(cwd, './packages')
 		};
 	}
 }
