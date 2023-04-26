f0b8b8b8e301bdd6b7a44178470ad935040c6bf3
Mr.Z <zrd0921@qq.com>
2023-04-27 01:55:48 +0800

    feat(index): export packages/*

--- packages/index/src/index.ts
+++ packages/index/src/index.ts
@@ -1,2 +1,8 @@
+import * as Builder from '@deot/dev-builder';
+import * as Releaser from '@deot/dev-releaser';
+import * as Tester from '@deot/dev-tester';
+
 export { Command } from '@deot/dev-test';
 export { Logger, Shell, Utils } from '@deot/dev-shared';
+
+export { Builder, Releaser, Tester };
