7d4d17790c16969dea81e540b5754c67c1f3d857
Mr.Z <zrd0921@qq.com>
2023-07-10 15:25:59 +0800

    feat(index): add `packages/updater`

--- packages/index/src/index.ts
+++ packages/index/src/index.ts
@@ -4,8 +4,9 @@ import * as Tester from '@deot/dev-tester';
 import * as Adder from '@deot/dev-adder';
 import * as Dever from '@deot/dev-dever';
 import * as Linker from '@deot/dev-linker';
+import * as Updater from '@deot/dev-updater';
 
 export { Command } from '@deot/dev-test';
 export { Logger, Shell, Utils } from '@deot/dev-shared';
 
-export { Builder, Releaser, Tester, Adder, Dever, Linker };
+export { Builder, Releaser, Tester, Adder, Dever, Linker, Updater };
