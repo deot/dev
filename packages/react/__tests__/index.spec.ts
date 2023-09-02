2828c25fa9a338ad91e95bc191d40f6cb830665e
Mr.Z <zrd0921@qq.com>
2023-09-02 18:06:19 +0800

    feat(react): react integration

---
+++ packages/react/__tests__/index.spec.ts
@@ -0,0 +1,42 @@
+import * as path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'fs-extra';
+import { build } from 'vite';
+import type { InlineConfig } from 'vite';
+import { Shell } from '@deot/dev-shared';
+import sharedConfig from '@deot/dev-react';
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+// @vitest-environment node
+describe('index.ts', () => {
+	const entry = path.resolve(dirname, './fixtures/app.tsx');
+
+	it('basic', () => {
+		expect(typeof sharedConfig).toBe('object');
+	});
+
+	it('build', async () => {
+		let options: InlineConfig = {
+			logLevel: 'silent',
+			build: {
+				write: false,
+				lib: {
+					entry,
+					formats: ["es"],
+					fileName: 'index.js'
+				}
+			}
+		};
+		options.configFile = path.resolve(dirname, '../src/index.ts');
+		let { output } = (await build(options))[0];
+		expect(typeof output[0].code === 'string').toBe(true);
+	});
+
+	it('tsc, *.d.ts', async () => {
+		fs.remove(path.resolve(dirname, './fixtures/dist'));
+		const has = (filename: string) => fs.existsSync(path.resolve(dirname, './fixtures/dist', filename));
+		await Shell.exec(`tsc`, ['-p', path.resolve(dirname, './fixtures/tsconfig.json')]);
+		expect(has('index.d.ts')).toBe(true);
+		expect(has('app.d.ts')).toBe(true);
+	}, 100000);
+});
