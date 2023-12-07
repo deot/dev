cb98871a805fb8ebb9838020c803b3c68127d957
Mr.Z <zrd0921@qq.com>
2023-12-07 16:44:54 +0800

    feat(stylelint): deprecated stylistic rules
    stylelint/stylelint#6342
    stylelint/stylelint#6409

--- packages/stylelint/__tests__/index.spec.ts
+++ packages/stylelint/__tests__/index.spec.ts
@@ -9,20 +9,77 @@ const lint = async (text: string) => {
 	return resultObject;
 };
 
-describe('index.ts', () => {
+describe('index.js', () => {
 	it('success', async () => {
 		expect.hasAssertions();
 		try {
-			const data = await lint(`a { color: red; }\n`);
+			const data = await lint(`a { color: inherit; }\n`);
 			expect(data.errored).toBe(false);
 		} catch (e) {
 			console.log(e);
 		}
 	});
 
-	it('declaration-block-trailing-semicolon', async () => {
+	it('single line comments', async () => {
 		expect.hasAssertions();
-		const data = await lint(`a { color: red }`);
-		expect(data.output).toMatch('declaration-block-trailing-semicolon');
+		const data = await lint(`// allow`);
+		expect(data.errored).toBe(false);
+	});
+
+	it('stylelint-config-standard-scss', async () => {
+		expect.hasAssertions();
+		const data = await lint(`
+			@use 'sass:meta';
+
+			@function color($value) {
+				@return if(meta.type-of($value) == 'string', $value, 'inherit');
+			}
+
+			a { color: color(1); }
+		`);
+		expect(data.errored).toBe(false);
+	});
+
+	it('unit-no-unknown, rpx', async () => {
+		expect.hasAssertions();
+		const data = await lint(`a { font-size: 12rpx; }`);
+		expect(data.errored).toBe(false);
+	});
+
+	it('unit-no-unknown, xxx disallow', async () => {
+		expect.hasAssertions();
+		const data = await lint(`a { font-size: 12xxx; }`);
+		expect(data.errored).toBe(true);
+	});
+
+	it('stylelint-order', async () => {
+		expect.hasAssertions();
+		const data = await lint(`a { bottom: 2px; top: 1px; }`);
+		expect(data.output).toMatch('Expected \\"top\\" to come before \\"bottom\\" (order/properties-order)');
 	});
 });
+
+/**
+ * https://stylelint.io/migration-guide/to-15
+ * 代码风格的提示已经移除, 现在都不会校正了;
+ * 包含
+ * 	1. 结尾需要分号
+ * 	2. 缩进
+ * 	3. ....
+ */
+describe('deprecated', () => {
+	it('semicolon', async () => {
+		expect.hasAssertions();
+		const data = await lint(`a { color: inherit }`);
+		expect(data.errored).toBe(false);
+	});
+
+	it('indentation', async () => {
+		expect.hasAssertions();
+		const data = await lint(`a {\n    color: inherit \n}`);
+		expect(data.errored).toBe(false);
+	});
+});
+
+
+
--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -1,9 +1,8 @@
 module.exports = {
 	root: true,
 	plugins: ['stylelint-order'],
-	extends: ['stylelint-config-standard'],
+	extends: ['stylelint-config-standard-scss'],
 	rules: {
-		"indentation": "tab",
 		'selector-pseudo-class-no-unknown': [
 			true,
 			{
@@ -32,12 +31,8 @@ module.exports = {
 		],
 		'no-empty-source': null,
 		'named-grid-areas-no-invalid': null,
-		'unicode-bom': 'never',
 		'no-descending-specificity': null,
 		'font-family-no-missing-generic-family-keyword': null,
-		'declaration-colon-space-after': 'always-single-line',
-		'declaration-colon-space-before': 'never',
-		'declaration-block-trailing-semicolon': 'always',
 		'rule-empty-line-before': [
 			'always',
 			{
