eaeb92c4dc1423bfd3b6b121084a6d59150cfb1a
Mr.Z <zrd0921@qq.com>
2023-12-18 17:19:40 +0800

    feat(test): add `Scheduler`

--- packages/test/src/index.ts
+++ packages/test/src/index.ts
@@ -5,9 +5,10 @@
 import { Command } from './command';
 import { Launch } from './launch';
 import { Operater } from './operater';
+import { Scheduler } from './scheduler';
 
 import * as E2E from './e2e';
 import * as Utils from './utils';
 import * as Server from './server';
 
-export { Command, E2E, Utils, Launch, Operater, Server };
+export { Command, E2E, Utils, Launch, Operater, Server, Scheduler };
